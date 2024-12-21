a6b49c12e7cfdd5697d4addf4fbf0f80afa9b143
Chuma Ez. <ezeoka0204@gmail.com>
2024-12-21 15:50:50 +0100

    Delete Results.R

--- Results.R
+++
@@ -1,5 +0,0 @@
-# Write results to CSV
-write.csv(as.data.frame(res), file = "deseq2_results.csv")
-
-# Write Ordered results to CSV
-write.csv(as.data.frame(resOrdered), file = "deseq2_resultsOrdered.csv")
